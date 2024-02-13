2fa7eac4c423e6e8fabaed6cde1b160cea74c1a3
Ashutosh9599 <ashutoshdas091@gmail.com>
2024-02-13 18:00:00 +0530

    Testing User Actions

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,44 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import store from './Store/Reducers/store';
+import { Provider } from 'react-redux';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('renders login screen by default', () => {
+  render(
+    <Provider store={store}>
+      <App />
+    </Provider>
+  );
+  const loginScreenElement = screen.getByText('Email:');
+  expect(loginScreenElement).toBeInTheDocument();
+});
+
+test('renders signup page when navigating to /signup', () => {
+  render(
+    <Provider store={store}>
+      <App />
+    </Provider>
+  );
+  const signupPageElement = screen.getByText('Password:');
+  expect(signupPageElement).toBeInTheDocument();
+});
+
+test('renders forgot password page when navigating to /forgot-password', () => {
+  render(
+    <Provider store={store}>
+      <App />
+    </Provider>
+  );
+  const forgotPasswordElement = screen.getByText('Forgot Password?');
+  expect(forgotPasswordElement).toBeInTheDocument();
+});
+
+test('renders welcome page after successful login', () => {
+  render(
+    <Provider store={store}>
+      <App />
+    </Provider>
+  );
+  const welcomePageElement = screen.getByText('Expense Tracker');
+  expect(welcomePageElement).toBeInTheDocument();
 });
